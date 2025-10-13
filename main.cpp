007977bf7fffef6f6a80b5a0e1e33ada315593ad
Marti Serrallonga <marti.serrallonga@fje.edu>
2025-10-13 13:51:50 +0200

    Break into functions

--- main.cpp
+++ main.cpp
@@ -12,13 +12,27 @@ std::vector<char> board{
 	'o','.','o','.','o','.','o','.',
 };
 
-int main() {
+void printBoard() {
 	for (int i = 0; i < board.size(); ++i) {
 		if (i % 8 == 0)
 			std::cout << '\n';
 
 		std::cout << ' ' << board.at(i);
 	}
+	std::cout << "\n";
+}
 
-	std::cout << "\n\n";
+void movePawn(int from, int to) {
+	board.at(to) = board.at(from);
+	board.at(from) = '.';
+}
+
+int main() {
+	printBoard();
+	movePawn(40, 33);
+
+	printBoard();
+	movePawn(40, 33);
+
+	printBoard();
 }
